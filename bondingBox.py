ab0456d10d5f389d6cd7ebfc06d6603ec2fa0dfc
= <=>
2023-06-16 19:50:14 +0700

    feat: add limit

--- bondingBox.py
+++ bondingBox.py
@@ -5,12 +5,12 @@ def box(img):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
     #Merah
-    red_low = np.array([0, 100, 100])
-    red_up = np.array([0, 255, 255])
+    red_low = np.array([141, 143, 21])
+    red_up = np.array([174, 255, 255])
 
     #Hijau
-    green_low = np.array([50, 100, 100])
-    green_up = np.array([70, 255, 255])
+    green_low = np.array([36, 0, 89])
+    green_up = np.array([120, 255, 255])
 
     #Biru
     blue_low = np.array([100, 100, 100])
@@ -25,27 +25,28 @@ def box(img):
     blue_mask = cv2.inRange(imgHSV, blue_low, blue_up)
     yellow_mask = cv2.inRange(imgHSV, yellow_low, yellow_up)    
 
-    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     #Merah
-    for cnt in contours:
+    contours_red, hierarchy = cv2.findContours(red_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours_red:
         area = cv2.contourArea(cnt)
         peri = cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
-   
         x, y, w, h = cv2.boundingRect(approx)
-        cv2.rectangle(img,(x,y), (x+w,y+h), (0,0,255), 3)
-        cv2.putText(img, 'RED BALL', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,0,255), 2)
+        if area > 100:
+            cv2.rectangle(img,(x,y), (x+w,y+h), (0,0,255), 3)
+            cv2.putText(img, 'RED BALL', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,0,255), 2)
 
     # Hijau
-    for cnt in contours:
+    contours_green, hierarchy = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours_green:
         area = cv2.contourArea(cnt)
         peri = cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
-   
         x, y, w, h = cv2.boundingRect(approx)
-        cv2.rectangle(img,(x,y), (x+w,y+h), (0,128, 0), 3)
-        cv2.putText(img, 'GREEN BALL', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,0,128), 2)
+        if area > 100:
+            cv2.rectangle(img,(x,y), (x+w,y+h), (0,128, 0), 3)
+            cv2.putText(img, 'GREEN BALL', (x,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.2, (0,128, 0), 2)
         
     return img
 
